f5e4e77a137a2fa2f6fdc0a16880795a4791713c
adriavc00 <adriavciscar@gmail.com>
2020-12-30 16:41:52 +0100

    Remove minimize, maximize and close buttons of the root stage.

--- src/meteonmea/MeteoNMEA.java
+++ src/meteonmea/MeteoNMEA.java
@@ -1,45 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package demoestacion;
+package meteonmea;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
+ * Main class of the application. It contains the launcher of the main graphical container.
  *
  * @author jsoler
+ * @author Adrià V. C.
+ * @author Felipe Z. M.
  */
-public class meteoNMEA extends Application {
+public class MeteoNMEA extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/FXMLMain.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/FXMLMain.fxml"));
 
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
         stage.setWidth(800);
         stage.setHeight(480);
-        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
-        stage.setOnCloseRequest(e -> {
+        stage.setOnCloseRequest((event) -> {
             System.exit(0);
         });
 
     }
 
     /**
+     * Method to be runned when the application starts. It launches the app.
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-
 }

d80ec288b6401c0224fd4453df9fc716e946510a
lipez <lipez@LAPTOP-C80I3VLN.home>
2021-01-24 17:01:56 +0100

    actions of the buttons, change scenes

--- src/controller/FXMLMainController.java
+++ src/controller/FXMLMainController.java
@@ -6,10 +6,12 @@ import java.time.LocalTime;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
@@ -27,6 +29,8 @@ public class FXMLMainController implements Initializable {
     private final Clock clock = new Clock();
 
     private Node numInfo;
+    private Node tempChart;
+    private Node windChart;
 
     @FXML
     private BorderPane mainPane;
@@ -40,6 +44,14 @@ public class FXMLMainController implements Initializable {
     private ToggleButton themeToggle;
     @FXML
     private Text statusText;
+    @FXML
+    private Button numericButton;
+    @FXML
+    private Button windButton;
+    @FXML
+    private Button tempButton;
+    @FXML
+    private Button pressureButton;
 
     /**
      * Initializes the controller class.
@@ -64,11 +76,28 @@ public class FXMLMainController implements Initializable {
             FXMLLoader customLoader = new FXMLLoader(
                     getClass().getResource("/view/FXMLNumericInfo.fxml"));
             this.numInfo = customLoader.load();
+            
+            customLoader = new FXMLLoader(
+                    getClass().getResource("/view/FXMLTempChart.fxml"));
+            
+            this.tempChart = customLoader.load();
+            
+            customLoader = new FXMLLoader(
+                    getClass().getResource("/view/FXMLWindChart.fxml"));
+            
+            this.windChart = customLoader.load();
+            
+            
         } catch (IOException ex) {
             Logger.getLogger(FXMLMainController.class.getName()).log(Level.SEVERE, null, ex);
         }
         // Set default center node
         mainPane.setCenter(numInfo);
+        
+        numericButton.setDisable(true);
+        tempButton.setDisable(false);
+        windButton.setDisable(false);
+        pressureButton.setDisable(false);
         // Bindings
         time.textProperty().bind(clock.timeProperty());
         date.textProperty().bind(clock.dateProperty());
@@ -92,6 +121,43 @@ public class FXMLMainController implements Initializable {
     @FXML
     private void exit(MouseEvent event) {
         ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
+    }
+
+    @FXML
+    private void numericScene(ActionEvent event) {
+        mainPane.setCenter(numInfo);
+        numericButton.setDisable(true);
+        tempButton.setDisable(false);
+        windButton.setDisable(false);
+        pressureButton.setDisable(false);
+    }
+
+    @FXML
+    private void windScene(ActionEvent event) {
+        mainPane.setCenter(windChart);
+        numericButton.setDisable(false);
+        tempButton.setDisable(false);
+        windButton.setDisable(true);
+        pressureButton.setDisable(false);
+    }
+
+    @FXML
+    private void tempScene(ActionEvent event) {
+        mainPane.setCenter(tempChart);
+        numericButton.setDisable(false);
+        tempButton.setDisable(true);
+        windButton.setDisable(false);
+        pressureButton.setDisable(false);
+    }
+
+    @FXML
+    private void pressureScene(ActionEvent event) {
+        //FALTA DE HACER EL FXML
+        
+        numericButton.setDisable(false);
+        tempButton.setDisable(false);
+        windButton.setDisable(false);
+        pressureButton.setDisable(true);
     }
 
 }
